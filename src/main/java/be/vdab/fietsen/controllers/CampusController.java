fa802864a38259c27f4dd8c1b39bb2ff44e893eb
npehlivanoglu <pehlivanoglu.np@gmail.com>
2023-03-24 09:06:00 +0100

    verzameling objecten

--- src/main/java/be/vdab/fietsen/controllers/CampusController.java
+++ src/main/java/be/vdab/fietsen/controllers/CampusController.java
@@ -1,24 +1,35 @@
 package be.vdab.fietsen.controllers;
 
+import be.vdab.fietsen.domain.Adres;
 import be.vdab.fietsen.domain.Campus;
+import be.vdab.fietsen.repositories.CampusRepository;
 import be.vdab.fietsen.services.CampusService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("campussen")
 public class CampusController {
     private final CampusService campusService;
 
+    private record CampusBeknopt(long id, String naam, Adres adres) {
+        CampusBeknopt(Campus campus) {
+            this(campus.getId(), campus.getNaam(), campus.getAdres());
+        }
+    }
+
     public CampusController(CampusService campusService) {
         this.campusService = campusService;
     }
 
     @GetMapping("westvlaams")
-    List<Campus> findWestVlaamse() {
-        return campusService.findWestVlaamse();
+    Stream<CampusBeknopt> findWestVlaamse() {
+        return campusService.findWestVlaamse()
+                .stream()
+                .map(campus -> new CampusBeknopt(campus));
     }
 }
--- src/main/java/be/vdab/fietsen/domain/Campus.java
+++ src/main/java/be/vdab/fietsen/domain/Campus.java
@@ -1,9 +1,10 @@
 package be.vdab.fietsen.domain;
 
-import jakarta.persistence.Embedded;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "campussen")
@@ -13,13 +14,28 @@ public class Campus {
     private String naam;
     @Embedded
     private Adres adres;
+    @ElementCollection
+    @CollectionTable(name = "huurprijzen",
+            joinColumns = @JoinColumn(name = "campusId"))
+    @OrderBy("vanaf")
+    private Set<Huurprijs> huurprijzen;
 
     public Campus(long id, String naam, Adres adres) {
         this.id = id;
         this.naam = naam;
         this.adres = adres;
+        huurprijzen = new LinkedHashSet<>();
     }
-    protected Campus(){};
+
+    public Set<Huurprijs> getHuurprijzen() {
+        return Collections.unmodifiableSet(huurprijzen);
+    }
+
+    protected Campus() {
+    }
+
+    ;
+
     public long getId() {
         return id;
     }
